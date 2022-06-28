49e74dfc2da95440bea2fc24bf53c46b3a3f5c02
Daiver500 <alexei.zenev@gmail.com>
2022-06-28 17:38:01 +0300

    Added button for load of cards (5+)

--- source/js/create-cards.js
+++ source/js/create-cards.js
@@ -1,6 +1,9 @@
 import {getData} from './server';
 
 const createCards = () => {
+
+  const MAX_CARDS = 5;
+
   getData('https://mocki.io/v1/a5814d24-4e22-49fc-96d1-0e9ae2952afc')
       .then((data) => {
         const newArray = [...data.articles];
@@ -30,7 +33,26 @@ const createCards = () => {
       item.content = item.content.slice(0, 200);
       createCard(item);
     });
+    const cardsToHide = document.querySelectorAll('.article-item:nth-child(n+7)');
+    cardsToHide .forEach((item) => {
+      item.classList.add('is-hidden');
+    });
   };
+
+  const showCards = () => {
+    const hiddenCards = document.querySelectorAll('.article-item.is-hidden');
+    for (let i = 0; i <= MAX_CARDS; i++) {
+      hiddenCards.forEach((item) => {
+        if (item.classList.contains('is-hidden')) {
+          hiddenCards[i].classList.remove('is-hidden');
+        }
+      });
+    }
+  };
+
+  const loadButton = document.querySelector('.articles__button');
+  loadButton.addEventListener('click', showCards);
+
 };
 
 export {createCards};
